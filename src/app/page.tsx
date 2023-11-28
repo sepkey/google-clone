19986db0af68edf3e30ce8d6cbbead23f596012c
Sepide Kia <sepidekia6990@gmail.com>
2023-11-28 22:41:33 +0330

    add home header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HomeHeader from '@/components/HomeHeader';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      sepide
-    </main>
+    <>
+      <HomeHeader />
+    </>
   );
 }
